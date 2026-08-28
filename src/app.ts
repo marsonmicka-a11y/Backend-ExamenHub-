d6cde8895e9db46cb31e99ed5e30e7dc3087a4bd
cedricpierre679-cell <cedricpierre679@gmail.com>
2026-08-28 11:54:04 +0300

    Clean up app.ts by removing commented code
    Removed commented sections for routes and server management.

--- src/app.ts
+++ src/app.ts
@@ -21,37 +21,23 @@ app.use(cors());
 app.use(express.json());
 
 
-// ========================
-// ROUTES
-// ========================
-
-// Connexion
 app.use("/api/auth", authRoutes);
 
-// Étudiants
 app.use("/api/students", studentRoutes);
 
-// Cours
 app.use("/api/courses", courseRoutes);
 
-// Examens
 app.use("/api/exams", examAdminRouter);
 
-// Questions
 app.use("/api/exams", examQuestionsRouter);
 app.use("/api/questions", questionsRouter);
 
-// Résultats
 app.use("/api/exams", examResultsRouter);
 app.use("/api/my", myResultsRouter);
 
-// Examens étudiant
 app.use("/api/my/exams", examStudentRouter);
 
 
-// ========================
-// TEST DU SERVEUR
-// ========================
 
 app.get("/api/health", (req, res) => {
   res.json({
@@ -60,25 +46,17 @@ app.get("/api/health", (req, res) => {
 });
 
 
-// ========================
-// ROUTE INEXISTANTE
-// ========================
+
 
 app.use("/api", (req, res) => {
   throw new ApiError(404, "Route introuvable");
 });
 
 
-// ========================
-// GESTION DES ERREURS
-// ========================
 
 app.use(errorMiddleware);
 
 
-// ========================
-// DÉMARRER LE SERVEUR
-// ========================
 
 app.listen(env.port, () => {
   console.log(
